65fa9c5bc48dd9508415ae751a34520162dd386e
selliens <stephan.sellien@uni-konstanz.de>
2013-10-28 17:45:14 +0100

    Removed old code from wrong first commit

--- org.knime.knip.tracking/src/org/knime/knip/trackingrevised/data/features/FeatureClass.java
+++ org.knime.knip.tracking/src/org/knime/knip/trackingrevised/data/features/FeatureClass.java
@@ -16,26 +16,31 @@ public abstract class FeatureClass {
 	 * @return the display name
 	 */
 	public abstract String getName();
-	
-	protected static double traverseConnectedDiffSV(TransitionGraph tg, CalculationSV calc) {
+
+	protected static double traverseConnectedDiffSV(TransitionGraph tg,
+			CalculationSV calc) {
 		try {
-			Partition firstPartition = tg.getNet().getPartition(tg.getFirstPartition());
-			Partition lastPartition = tg.getNet().getPartition(tg.getLastPartition());
-			WeakConnectedComponent wcc = new WeakConnectedComponent(new ExecutionMonitor(), tg.getNet());
+			Partition firstPartition = tg.getNet().getPartition(
+					tg.getFirstPartition());
+			Partition lastPartition = tg.getNet().getPartition(
+					tg.getLastPartition());
+			WeakConnectedComponent wcc = new WeakConnectedComponent(
+					new ExecutionMonitor(), tg.getNet());
 			wcc.start(new ExecutionMonitor());
-			for(KPartiteGraph<PersistentObject, Partition> cc : wcc.createComponentViews(new ExecutionMonitor(), tg.getNet())) {
+			for (KPartiteGraph<PersistentObject, Partition> cc : wcc
+					.createComponentViews(new ExecutionMonitor(), tg.getNet())) {
 				double fpsum = 0.0;
-				for(PersistentObject po : cc.getNodes(firstPartition)) {
+				for (PersistentObject po : cc.getNodes(firstPartition)) {
 					double newNumber = calc.calculate(po);
 					fpsum = calc.sum(fpsum, newNumber);
 				}
-				
+
 				double lpsum = 0.0;
-				for(PersistentObject po : cc.getNodes(lastPartition)) {
+				for (PersistentObject po : cc.getNodes(lastPartition)) {
 					double newNumber = calc.calculate(po);
 					lpsum = calc.sum(lpsum, newNumber);
 				}
-				
+
 				return calc.diff(fpsum, lpsum);
 			}
 		} catch (PersistenceException e) {
@@ -45,10 +50,9 @@ public abstract class FeatureClass {
 		}
 		return 0.0;
 	}
-	
+
 	protected static abstract class CalculationSV {
-		
-		
+
 		public double sum(double sum, double newValue) {
 			return sum + newValue;
 		}
@@ -59,37 +63,46 @@ public abstract class FeatureClass {
 
 		public abstract double calculate(PersistentObject po);
 	}
-	
-	protected static double traverseConnectedDiffMV(TransitionGraph tg, CalculationMV calc) {
+
+	protected static double traverseConnectedDiffMV(TransitionGraph tg,
+			CalculationMV calc) {
 		try {
-			Partition firstPartition = tg.getNet().getPartition(tg.getFirstPartition());
-			Partition lastPartition = tg.getNet().getPartition(tg.getLastPartition());
-			WeakConnectedComponent wcc = new WeakConnectedComponent(new ExecutionMonitor(), tg.getNet());
+			Partition firstPartition = tg.getNet().getPartition(
+					tg.getFirstPartition());
+			Partition lastPartition = tg.getNet().getPartition(
+					tg.getLastPartition());
+			WeakConnectedComponent wcc = new WeakConnectedComponent(
+					new ExecutionMonitor(), tg.getNet());
 			wcc.start(new ExecutionMonitor());
-			for(KPartiteGraph<PersistentObject, Partition> cc : wcc.createComponentViews(new ExecutionMonitor(), tg.getNet())) {
+			for (KPartiteGraph<PersistentObject, Partition> cc : wcc
+					.createComponentViews(new ExecutionMonitor(), tg.getNet())) {
 				int count = 0;
 				double[] fpsum = null;
-				for(PersistentObject po : cc.getNodes(firstPartition)) {
+				for (PersistentObject po : cc.getNodes(firstPartition)) {
 					double[] newNumber = calc.calculate(po);
 					fpsum = calc.sum(fpsum, newNumber);
 					count++;
 				}
-				
-				for(int i = 0; i < fpsum.length; i++) {
-					fpsum[i] /= count;
+
+				if (fpsum != null) {
+					for (int i = 0; i < fpsum.length; i++) {
+						fpsum[i] /= count;
+					}
 				}
-				
+
 				count = 0;
 				double[] lpsum = null;
-				for(PersistentObject po : cc.getNodes(lastPartition)) {
+				for (PersistentObject po : cc.getNodes(lastPartition)) {
 					double[] newNumber = calc.calculate(po);
 					lpsum = calc.sum(lpsum, newNumber);
 				}
-				
-				for(int i = 0; i < lpsum.length; i++) {
-					lpsum[i] /= count;
+
+				if (lpsum != null) {
+					for (int i = 0; i < lpsum.length; i++) {
+						lpsum[i] /= count;
+					}
 				}
-				
+
 				return calc.diff(fpsum, lpsum);
 			}
 		} catch (PersistenceException e) {
@@ -99,25 +112,30 @@ public abstract class FeatureClass {
 		}
 		return 0.0;
 	}
-	
-protected static abstract class CalculationMV {
-		
-		
+
+	protected static abstract class CalculationMV {
+
 		public double[] sum(double[] sum, double newValue[]) {
-			if(sum == null) {
+			if (sum == null) {
 				sum = new double[newValue.length];
 			}
-			for(int i = 0; i < sum.length; i++) {
+			for (int i = 0; i < sum.length; i++) {
 				sum[i] += newValue[i];
 			}
 			return sum;
 		}
 
 		public double diff(double[] fpsum, double[] lpsum) {
-			//euclidean
+			// euclidean
 			double dist = 0.0;
-			for(int i = 0; i < fpsum.length; i++) {
-				dist += (fpsum[i]*fpsum[i])-(lpsum[i]*lpsum[i]);
+			if(fpsum == null && lpsum == null)
+				return 0.0;
+			if(fpsum == null) 
+				fpsum = new double[lpsum.length];
+			if(lpsum == null)
+				lpsum = new double[fpsum.length];
+			for (int i = 0; i < fpsum.length; i++) {
+				dist += (fpsum[i] * fpsum[i]) - (lpsum[i] * lpsum[i]);
 			}
 			dist = Math.sqrt(dist);
 			return dist;
